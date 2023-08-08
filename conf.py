e4dedcc12f762946de35b1c1b12b5778816f11d0
Damien Ayers <damien@omad.net>
2023-08-08 11:59:25 +1000

    Setup redirects for page renames
    And also remove useless sections of the config

--- conf.py
+++ conf.py
@@ -184,82 +184,17 @@ html_favicon = "_static/dea-favicon.ico"
 
 html_use_index = True
 
-# -- Options for HTMLHelp output ------------------------------------------
 
-# Output file base name for HTML help builder.
-htmlhelp_basename = "DigitalEarthAustraliadoc"
+rediraffe_redirects = "redirects.txt"
+rediraffe_branch = "master~1"
 
-
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (
-        master_doc,
-        "DigitalEarthAustralia.tex",
-        "Digital Earth Australia Documentation",
-        "Geoscience Australia",
-        "manual",
-    ),
-]
-
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (
-        master_doc,
-        "digitalearthaustralia",
-        "Digital Earth Australia Documentation",
-        [author],
-        1,
-    )
-]
-
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (
-        master_doc,
-        "DigitalEarthAustralia",
-        "Digital Earth Australia Documentation",
-        author,
-        "DigitalEarthAustralia",
-        "One line description of project.",
-        "Miscellaneous",
-    ),
-]
 
 spelling_word_list_filename = 'notebooks/.github/workflows/configs/spellcheck_wordlist.txt'
 
-# # Example configuration for intersphinx: refer to the Python standard library.
-# intersphinx_mapping = {
-#     "python": ("https://docs.python.org/3/", None),
-#     "odc": ("https://datacube-core.readthedocs.io/en/stable/", None),
-# }
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "odc": ("https://datacube-core.readthedocs.io/en/stable/", None),
+}
 
 
 def setup(app):
