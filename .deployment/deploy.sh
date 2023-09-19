b1969b2437b9bc6b31d0626d8f09ebe84cd80817
Benji Glitsos <benji.glitsos@ga.gov.au>
2023-09-19 13:41:21 +1000

    Removed all data from this branch to start from a blank slate

--- .deployment/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Deploy the docs to S3 and invalidate the cloudfront distribution
-#
-
-pip install awscli
-aws s3 sync --delete _build/html/ s3://docs.dea.ga.gov.au
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths '/*'
