6a539054a69ec2052ae46a0f5da88d4215325534
Benji Glitsos <benji.glitsos@ga.gov.au>
2023-11-14 10:57:07 +1100

    Adding knowledge hub redirects and setting up other redirects files

--- docs/conf.py
+++ docs/conf.py
@@ -69,14 +69,13 @@ nbsphinx_execute = "never"
 external_toc_path = "table_of_contents.yaml"
 
 if os.environ.get("ENABLE_REDIRECTS") == "Yes" or os.environ.get("PRODUCTION_MODE") == "Yes":
-    redirects = {}
+    rediraffe_redirects = {}
     for redirects_file in glob.glob("_redirects/*.txt"):
         with open(redirects_file, "r") as redirects:
             for line in redirects:
                 if not line.startswith("# "):
                     from_file, to_file = re.match(r"\"?([^\"]*)\"?\s*\"?([^\"]*)\"?", line).groups()
-                    redirects[from_file] = to_file
-    rediraffe_redirects = redirects
+                    rediraffe_redirects[from_file] = to_file
 
 sitemap_url_scheme = "{link}"
 
