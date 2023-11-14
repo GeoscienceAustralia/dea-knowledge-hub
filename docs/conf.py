05f81fa03c76e84f63921b294f4d52af4291707c
Benji Glitsos <benji.glitsos@ga.gov.au>
2023-11-14 12:43:38 +1100

    Python module import is working but not in subfolder

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,7 @@
-import os
 import sys
-import abc.utilities
+import os
+sys.path.insert(0, os.path.abspath('.'))
+import utilities
 
 project = "DEA Docs"
 copyright = f"{utilities.current_year()}, Geoscience Australia"
