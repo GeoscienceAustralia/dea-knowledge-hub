e0eb1d8caca5e92b85087c73d3f1ba5c52b16b56
Benji Glitsos <benji.glitsos@ga.gov.au>
2023-11-16 16:45:00 +1100

    Updated Dockerfile to align with the new Netlify build system

--- docker-start.sh
+++ docker-start.sh
@@ -2,12 +2,21 @@
 
 rm -r /output/*
 
-sass --style=compact /docs/_static/styles/index.scss /docs/_static/styles/styles.css
+if [ "$PRODUCTION_MODE" == "Yes" ]
+then
+    sass --style=compressed /docs/_static/styles/index.scss /docs/_static/styles/styles.css
+else
+    sass --style=expanded /docs/_static/styles/index.scss /docs/_static/styles/styles.css
+fi
+
+if [ "$PRODUCTION_MODE" == "Yes" ]
+then
+    cp /docs/_robots/robots-prod.txt /docs/robots.txt
+else
+    cp /docs/_robots/robots-dev.txt /docs/robots.txt
+fi
 
 sphinx-build -b dirhtml -j auto /docs /output
 
-if [ "$PRODUCTION_MODE" == "No" ]
-then
-    cd /output
-    python3 -m http.server 8011
-fi
+cd /output
+python3 -m http.server 8011
