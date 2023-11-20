dcc94973176029279cf4f490c5739319ab3d07b6
Benji Glitsos <benji.glitsos@ga.gov.au>
2023-11-20 12:01:50 +1100

    Wording of code comment

--- deploy-production.sh
+++ deploy-production.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Deploy the docs to S3 and invalidate the Cloudfront distribution
+# Deploy to S3 and invalidate the CloudFront distribution
 
 python -m pip install awscli
 aws s3 sync --delete output s3://docs.dea.ga.gov.au
