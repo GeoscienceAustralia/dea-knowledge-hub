2d64559fabffa85bb58a777281e37efb07c70353
Benji Glitsos <benji.glitsos@ga.gov.au>
2023-12-05 13:12:53 +1100

    Moved installing the requirements into its own step in the production workflow

--- build-production.sh
+++ build-production.sh
@@ -2,13 +2,6 @@
 
 set -eox pipefail
 
-python -m pip install --upgrade pip
-python -m pip install -r ./requirements.txt
-
-npm install -g sass
-
-sudo apt-get install -y pandoc
-
 mkdir -p ./output
 
 sass --style=compressed ./docs/_static/styles/index.scss ./docs/_static/styles/styles.css
