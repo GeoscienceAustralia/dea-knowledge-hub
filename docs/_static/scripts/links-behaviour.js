a5d5d7425b42a02819bb33db10c9c3d4107bad1c
Benji Glitsos <benji.glitsos@ga.gov.au>
2023-12-22 17:36:37 +1100

    Adding icons to external links and making the anchor link logic work

--- docs/_static/scripts/links-behaviour.js
+++ docs/_static/scripts/links-behaviour.js
@@ -1,17 +1,18 @@
-// Makes all external links open in a new tab
+// Classifies each type of link (anchor, internal, external) with a CSS class and also makes external links open in a new tab by default.
 
 document.addEventListener("DOMContentLoaded", function (event) {
     for (var links = document.links, i = 0, a; (a = links[i]); i++) {
-        if (a.host === location.host) {
+        var hrefAnchor = a.href.replace(location.href, "");
+        if (/^#/.test(hrefAnchor)) {
+            a.classList.add("anchor-link");
+        } else if (a.host === location.host) {
             a.classList.add("internal-link");
-        } else if (a.host !=== location.host) {
+        } else if (a.host !== location.host) {
             a.classList.add("external-link");
             if (!a.target) {
                 a.target = "_blank";
                 a.setAttribute("rel", "noopener noreferrer");
             }
-        } else if (/^#/.test(a.href)) {
-            a.classList.add("anchor-link");
         }
     }
 });
