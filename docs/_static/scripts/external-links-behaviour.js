5699ebcfe40c63e7b4226e2a73e577c6c8d84270
Benji Glitsos <benji.glitsos@ga.gov.au>
2023-12-22 16:37:29 +1100

    Modified the external links behaviour script to allow for customising the link behaviour per link

--- docs/_static/scripts/external-links-behaviour.js
+++ docs/_static/scripts/external-links-behaviour.js
@@ -2,10 +2,10 @@
 
 document.addEventListener("DOMContentLoaded", function (event) {
     for (var links = document.links, i = 0, a; (a = links[i]); i++) {
-        if (a.host !== location.host) {
+        if (a.host !== location.host && !a.target) {
             a.target = "_blank";
             a.setAttribute("rel", "noopener noreferrer");
-            a.classList.add("external");
+            a.classList.add("open-in-new-tab");
         }
     }
 });
