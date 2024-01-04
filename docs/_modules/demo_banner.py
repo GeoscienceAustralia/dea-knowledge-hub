e61f3c18eef65e6d9acdc0c86b5d3721f0f85b2a
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-01-04 17:31:58 +1100

    Editing the demo banner to make it more user-friendly

--- docs/_modules/demo_banner.py
+++ docs/_modules/demo_banner.py
@@ -1,2 +1,8 @@
 def create(demo_name, git_branch, deploy_id):
-    return f"You are viewing <strong>{demo_name}</strong> on <a href=\"https://github.com/GeoscienceAustralia/dea-docs/tree/{git_branch}\">{git_branch}</a> branch. <a href=\"https://app.netlify.com/sites/dea-docs/deploys/{deploy_id}\">View the build log</a>. <a href=\"https://docs.dea.ga.gov.au/\" target=\"_self\">Go to the main website</a>."
+    alert_text = f'<strong>Note:</strong> You are viewing <strong>{demo_name}</strong> not the <a href="https://docs.dea.ga.gov.au/">official website</a>.'
+    menu_text = '<strong>For staff:</strong>'
+    git_branch_text = f'On <a href="https://github.com/GeoscienceAustralia/dea-docs/tree/{git_branch}">{git_branch}</a> branch.'
+    build_logs_text = f'View the <a href="https://app.netlify.com/sites/dea-docs/deploys?filter={git_branch}">build logs</a>.'
+
+    return " ".join([alert_text, menu_text, git_branch_text, build_logs_text])
+
