d5ca605c967daca57d2ee6377c241c2020c9d9f8
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-01-11 13:17:32 +1100

    Adapting this script from a script I found on Github: https://github.com/readthedocs/readthedocs.org/blob/main/docs/_static/js/expand_tabs.js

--- docs/_static/scripts/product-tab-deep-links.js
+++ docs/_static/scripts/product-tab-deep-links.js
@@ -1,3 +1,16 @@
 // Enable the tabs on product pages to be linked to specifically. E.g. ?tab=access will open the Access tab.
 
-document.addEventListener("DOMContentLoaded", function (event) {});
+document.addEventListener("DOMContentLoaded", function (event) {
+    const tabUrlParameter = new URLSearchParams(window.location.search).get(
+        "tab"
+    );
+
+    if (tabUrlParameter !== null) {
+        const items = document.querySelectorAll("a.item > div");
+        items.forEach(function (item) {
+            if (item.textContent.includes(tabName)) {
+                item.parentElement.click();
+            }
+        });
+    }
+});
