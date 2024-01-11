7d03557f3f9ddd545bde24fe9bd7f6d72b05b557
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-01-11 16:01:05 +1100

    Revert "Working on using URL hash fragments rather than querystring parameters"
    This reverts commit c3a053cd84c446741dae8dc24e3588cc28613dda.

--- docs/_static/scripts/product-tab-deep-links.js
+++ docs/_static/scripts/product-tab-deep-links.js
@@ -1,19 +1,12 @@
 // Enable linking to a specific tab on a product page by adding a parameter to the URL.
 // E.g. to open the Access tab, use ?tab=access
 
-function selectTab(tabId) {
-    return document.querySelector(`.sd-tab-set > ${tabId}`)
-}
-
 document.addEventListener("DOMContentLoaded", function (event) {
-    const fragment = window.location.hash;
-    const tabParameter = new URLSearchParams(window.location.search).get(
+    const tabUrlParameter = new URLSearchParams(window.location.search).get(
         "tab"
     );
 
-    if(selectTab(fragment)) {
-        selectTab(fragment).click();
-    } else if(selectTab(`#${tabParameter}`)) {
-        selectTab(`#${tabParameter}`).click();
+    if (tabUrlParameter) {
+        document.querySelector(`.sd-tab-set > #${tabUrlParameter}`).click();
     }
 });
--- docs/_templates/product-v1.rst
+++ docs/_templates/product-v1.rst
@@ -117,7 +117,7 @@
        .. rubric:: Access the data
           :name: access-the-data
 
-       For help accessing the data, see the `Access tab <./#access>`_.
+       For help accessing the data, see the 'Access' tab.
 
        .. container:: card-list icons
           :name: access-the-data-cards
