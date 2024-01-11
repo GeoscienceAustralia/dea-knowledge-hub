486c8d699630f32dec97670a7f4b3e822b1f7588
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-01-11 16:00:32 +1100

    Revert "Implementing the ability to use a dot in the hash e.g. details.background"
    This reverts commit a6da8edc39f0246d9b678f968d62d534ab9df717.

--- docs/_static/scripts/product-tab-deep-links.js
+++ docs/_static/scripts/product-tab-deep-links.js
@@ -1,20 +1,19 @@
 // Enable linking to a specific tab on a product page by adding a parameter to the URL.
 // E.g. to open the Access tab, use ?tab=access
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    const hash = window.location.hash;
-    const parsedHash = hash.match(/#([^.]+).?([^.]*)/);
-    const tab = parsedHash[1];
-    const heading = parsedHash[2];
-
-    if (tab) {
-        const tabElement = document.querySelector(`.sd-tab-set > #${tab}`);
-
-        tabElement && tabElement.click();
-
-        if (heading) {
-            window.location.hash = heading;
-            window.location.hash = hash;
-        }
-    }
-});
+// function selectTab(tabId) {
+//     return document.querySelector(`.sd-tab-set > ${tabId}`)
+// }
+//
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     const fragment = window.location.hash;
+//     const tabParameter = new URLSearchParams(window.location.search).get(
+//         "tab"
+//     );
+//
+//     if(selectTab(fragment)) {
+//         selectTab(fragment).click();
+//     } else if(selectTab(`#${tabParameter}`)) {
+//         selectTab(`#${tabParameter}`).click();
+//     }
+// });
