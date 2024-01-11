26b96604be2e95b2aed3a8275a8837f10344c62b
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-01-11 14:03:14 +1100

    Deep linking to a tab is working

--- docs/_static/scripts/product-tab-deep-links.js
+++ docs/_static/scripts/product-tab-deep-links.js
@@ -1,16 +1,12 @@
-// Enable the tabs on product pages to be linked to specifically. E.g. ?tab=access will open the Access tab.
+// Enable linking to a specific tab on a product page by adding a parameter to the URL.
+// E.g. to open the Access tab, use ?tab=access
 
 document.addEventListener("DOMContentLoaded", function (event) {
     const tabUrlParameter = new URLSearchParams(window.location.search).get(
         "tab"
     );
 
-    if (tabUrlParameter !== null) {
-        const items = document.querySelectorAll("a.item > div");
-        items.forEach(function (item) {
-            if (item.textContent.includes(tabName)) {
-                item.parentElement.click();
-            }
-        });
+    if (tabUrlParameter) {
+        document.querySelector(`.sd-tab-set > #${tabUrlParameter}`).click();
     }
 });
