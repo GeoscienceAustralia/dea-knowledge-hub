ee20ac79b86163a6c307940eda7c4a6f34a5855c
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-01-25 14:59:07 +1100

    [skip ci] Changed from using BUILD_ID to DEPLOY_ID to try to get the deploy link to work

--- docs/_modules/deploy_banner.py
+++ docs/_modules/deploy_banner.py
@@ -5,13 +5,13 @@ def banner():
     deploy_name = os.environ.get("DEPLOY_NAME")
     pull_request = os.environ.get("PULL_REQUEST")
     review_id = os.environ.get("REVIEW_ID")
-    build_id = os.environ.get("BUILD_ID")
+    deploy_id = os.environ.get("DEPLOY_ID")
 
     knowledge_hub_url = "https://docs.dea.ga.gov.au/"
     github_url = "https://github.com/GeoscienceAustralia/dea-docs"
     deploy_logs_url = "https://app.netlify.com/sites/dea-docs/deploys"
 
-    pull_request_banner = f'You are viewing <strong>{deploy_name} #{review_id}</strong>, not the official <a href="{knowledge_hub_url}">DEA Knowledge Hub</a>. View the <a href="{deploy_logs_url}/{build_id}">deploy log</a> or <a href="{github_url}/pull/{review_id}">pull request</a>.'
+    pull_request_banner = f'You are viewing <strong>{deploy_name} #{review_id}</strong>, not the official <a href="{knowledge_hub_url}">DEA Knowledge Hub</a>. View the <a href="{deploy_logs_url}/{deploy_id}">deploy log</a> or <a href="{github_url}/pull/{review_id}">pull request</a>.'
 
     demo_banner = f'You are viewing <strong>{deploy_name}</strong>, not the official <a href="{knowledge_hub_url}">DEA Knowledge Hub</a>. View the <a href="{deploy_logs_url}?filter={git_branch}">deploy logs</a> or <a href="{github_url}/tree/{git_branch}">git branch</a>.'
 
