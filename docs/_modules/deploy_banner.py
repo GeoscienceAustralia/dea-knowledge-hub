018f4a3b9049ec06a2adf829979f65219bca36ba
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-01-25 14:39:18 +1100

    Refactoring deploy banner code

--- docs/_modules/deploy_banner.py
+++ docs/_modules/deploy_banner.py
@@ -1,18 +1,22 @@
 import os
 
 def banner():
+    git_branch = os.environ.get("BRANCH")
+    deploy_name = os.environ.get("DEPLOY_NAME")
     pull_request = os.environ.get("PULL_REQUEST")
     review_id = os.environ.get("REVIEW_ID")
     build_id = os.environ.get("BUILD_ID")
-    git_branch = os.environ.get("BRANCH")
-    deploy_name = os.environ.get("DEPLOY_NAME")
 
     knowledge_hub_url = "https://docs.dea.ga.gov.au/"
     github_url = "https://github.com/GeoscienceAustralia/dea-docs"
     deploy_logs_url = "https://app.netlify.com/sites/dea-docs/deploys"
 
+    pull_request_banner = f'You are viewing <strong>{deploy_name}</strong>, not the official <a href="{knowledge_hub_url}">DEA Knowledge Hub</a>. View the <a href="{deploy_logs_url}?filter={git_branch}">deploy logs</a> or <a href="{github_url}/tree/{git_branch}">git branch</a>.'
+
+    demo_banner = f'You are viewing <strong>Pull Request #{review_id}</strong>, not the official <a href="{knowledge_hub_url}">DEA Knowledge Hub</a>. View the <a href="{deploy_logs_url}/{build_id}">latest deploy log</a> or the <a href="{github_url}/pull/{review_id}">pull request</a>.'
+
     if pull_request == "true":
-        return f'You are viewing <strong>{deploy_name}</strong>, not the <a href="{knowledge_hub_url}">main website</a>. View the <a href="{deploy_logs_url}?filter={git_branch}">deploy logs</a> or <a href="{github_url}/tree/{git_branch}">git branch</a>.'
+        return pull_request_banner
     else:
-        return f'You are viewing <strong>PR #{review_id} {deploy_name}</strong>, not the <a href="{knowledge_hub_url}">main website</a>. View the <a href="{deploy_logs_url}/{build_id}">latest deploy log</a> or the <a href="{github_url}/pull/{review_id}">PR</a>.'
+        return demo_banner
 
