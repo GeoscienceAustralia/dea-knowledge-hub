e7de79a538de1f2c5589a6ac6da084a6b8a15df1
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-01-29 14:38:51 +1100

    Standardising all heading formats across the site in order to make this script work globally

--- docs/_static/scripts/clickable-heading-anchors.js
+++ docs/_static/scripts/clickable-heading-anchors.js
@@ -1,16 +1,43 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    const headings = document.querySelectorAll("h2[id], h3[id], h4[id]"); // section[id]
+    // Move the section IDs to the headings
+
+    let sections = document.querySelectorAll("section[id]");
+
+    for (let i = 0; i < sections.length; i++) {
+        let section = sections[i];
+        let id = section.id;
+        section.removeAttribute("id");
+        section.querySelector("* > h2, * > h3, * > h4").id = id;
+    }
+
+    // Convert the 'rubrics' to H2 headings
+
+    let rubrics = document.querySelectorAll("p.rubric");
+
+    for (let i = 0; i < rubrics.length; i++) {
+        let rubric = rubrics[i];
+        let h2 = document.createElement("h2");
+        h2.id = rubric.id;
+        h2.class = rubric.class;
+        h2.innerHTML = rubric.innerHTML;
+        rubric.parentNode.replaceChild(h2, rubric);
+    }
+
+    // Make the headings clickable
+
+    let headings = document.querySelectorAll("h2[id], h3[id], h4[id]"); // section[id]
 
     for (var i = 0; i < headings.length; i++) {
-        const heading = headings[i];
-        const anchorId = heading.id;
+        let heading = headings[i];
+        let anchorId = heading.id;
 
         heading.classList.add("clickable-anchor");
 
         if (anchorId) {
             heading.addEventListener("click", function() {
-                    window.location.hash = `#${anchorId}`
+                window.location.hash = `#${anchorId}`
             });
         }
     }
 });
+
