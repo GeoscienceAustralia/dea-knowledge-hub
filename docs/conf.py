b5aa2de37a13488c16e90ddbd951613e10174cb4
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-01-31 16:25:52 +1100

    Trying to fix bugs

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,7 @@ exclude_patterns = [
 exclude_patterns += utilities.optional_exclude_pattern("LOCAL_ENABLE_USER_GUIDES", "guides")
 exclude_patterns += utilities.optional_exclude_pattern("LOCAL_ENABLE_DATA_PRODUCTS", "data")
 exclude_patterns += utilities.optional_exclude_pattern("LOCAL_ENABLE_NOTEBOOKS", "notebooks")
+exclude_patterns += utilities.optional_exclude_pattern("LOCAL_ENABLE_TAGS", "tags")
 
 html_title = "DEA Knowledge Hub"
 html_logo = "_files/logos/ga-dea-combined-logo.svg"
