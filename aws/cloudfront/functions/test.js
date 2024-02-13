28fc3ae43304fa349dd0e6f50b48f65dcf606028
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-02-13 12:27:04 +1100

    Adding more test cases

--- aws/cloudfront/functions/test.js
+++ aws/cloudfront/functions/test.js
@@ -4,16 +4,41 @@ var assert = require("assert");
 
 describe("handler()", () => {
     const tests = [
+        // Should redirect correctly
+
         { uri: "/index.html", expected: "/" },
         { uri: "/page/index.html", expected: "/page/" },
-        { uri: "/example.html", expected: "/example/" },
-        { uri: "/foo/example.rst", expected: "/foo/example/" },
+        { uri: "/category/page.html", expected: "/category/page/" },
+        { uri: "/page.html", expected: "/page/" },
+        { uri: "/page.rst", expected: "/page/" },
+        { uri: "/page.md", expected: "/page/" },
+        { uri: "/page.ipynb", expected: "/page/" },
+        { uri: "/page.py", expected: "/page/" },
         {
             uri: "/notebooks/Beginners_guide/Tools/dea_tools/plotting.py",
             expected: "/notebooks/Tools/gen/dea_tools.plotting/"
         },
         {
             uri: "/notebooks/Interactive_apps/Tools/dea_tools/app/animations.py",
+            expected: "/notebooks/Tools/gen/dea_tools.app.animations/"
+        },
+
+        // Should not redirect
+
+        {
+            uri: "/data/product/dea-coastlines/",
+            expected: "/data/product/dea-coastlines/"
+        },
+        {
+            uri: "/data/product/dea-coastlines/?tab=overview",
+            expected: "/data/product/dea-coastlines/?tab=overview"
+        },
+        {
+            uri: "/notebooks/Tools/gen/dea_tools.plotting/",
+            expected: "/notebooks/Tools/gen/dea_tools.plotting/"
+        },
+        {
+            uri: "/notebooks/Tools/gen/dea_tools.app.animations/",
             expected: "/notebooks/Tools/gen/dea_tools.app.animations/"
         }
     ];
