8692756b10d7f8b0a4a6b260e24896065dddf026
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-02-20 12:36:22 +1100

    Wrote subdomain redirect logic

--- aws/cloudfront/functions/redirects.js
+++ aws/cloudfront/functions/redirects.js
@@ -2,15 +2,32 @@
 
 async function handler(event) {
     const request = event.request;
+    const host = event.request.headers.host.value;
     const uri = event.request.uri;
+
     const deaToolsSourceCodePattern = /dea_tools\/(.*)\.py$/;
     const indexHtmlPattern = /\/index\.html$/g;
     const filetypeExtensionsPattern = /\.(html|rst|md|ipynb|py)$/g;
+
+    const docsHost = "docs";
+    const knowledgeHost = "knowledge";
 
     const status301MovedPermanently = {
         statusCode: 301,
         statusDescription: "Moved Permanently"
     };
+
+    // Redirect subdomain from 'docs' to 'knowledge'. E.g. "docs.dea.ga.gov.au" => "knowledge.dea.ga.gov.au"
+
+    if (host === docsHost) {
+        return Object.assign(status301MovedPermanently, {
+            headers: {
+                location: {
+                    value: "https://" + knowledgeHost + uri
+                }
+            }
+        });
+    }
 
     // Redirect DEA Tools source code URLs to the relevant 'automodule' page generated from the source code.
     // E.g. "/notebooks/Beginners_guide/Tools/dea_tools/plotting.py" => "/notebooks/Tools/gen/dea_tools.plotting/"
