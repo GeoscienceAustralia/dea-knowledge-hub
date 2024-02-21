6a4166788e1b6180f87e85a13fdaa23ed59369ec
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-02-22 10:27:09 +1100

    Tidied up the mock imports code

--- docs/_modules/mock_imports.py
+++ docs/_modules/mock_imports.py
@@ -4,15 +4,15 @@ mock_imports_file = ".mock-imports"
 comment_symbol = "#"
 
 mock_imports = []
+project_root = os.path.dirname(os.path.dirname(__file__))
+mock_imports_file_path = os.path.join(project_root, mock_imports_file)
 
 try:
-    project_root = os.path.dirname(os.path.dirname(__file__))
-    mock_imports_file_path = os.path.join(project_root, mock_imports_file)
     with open(mock_imports_file_path, 'r') as file:
-        mock_imports = file.read().splitlines()
-        mock_imports = [line.strip() for line in mock_imports] # Ignore blank lines
-        mock_imports = [line for line in mock_imports if not line.startswith(comment_symbol)] # Ignore commented lines
-        print(mock_imports)
+        lines = file.read().splitlines()
+        lines = [line.strip() for line in mock_imports] # Ignore blank lines
+        lines = [line for line in mock_imports if not line.startswith(comment_symbol)] # Ignore commented lines
+        mock_imports = lines
 except FileNotFoundError:
     print(f"File '{mock_imports_file}' not found in root folder of project.")
 
