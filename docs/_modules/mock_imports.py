b148c41716928f3511027eb338d1c414b493384f
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-02-22 10:49:45 +1100

    Mock imports module now working

--- docs/_modules/mock_imports.py
+++ docs/_modules/mock_imports.py
@@ -10,9 +10,11 @@ mock_imports_file_path = os.path.join(project_root, mock_imports_file)
 try:
     with open(mock_imports_file_path, 'r') as file:
         lines = file.read().splitlines()
-        lines = [line.strip() for line in mock_imports] # Ignore blank lines
-        lines = [line for line in mock_imports if not line.startswith(comment_symbol)] # Ignore commented lines
+        lines = [line.strip() for line in lines] # Strip leading and trailing whitespace from each line
+        lines = [line for line in lines if line] # Ignore blank lines
+        lines = [line for line in lines if not line.startswith(comment_symbol)] # Ignore commented lines
         mock_imports = lines
+        print(f"Sourced {len(mock_imports)} mock imports from '{mock_imports_file_path}'")
 except FileNotFoundError:
-    print(f"File '{mock_imports_file}' not found in root folder of project.")
+    print(f"Mock imports file not found at '{mock_imports_file_path}'")
 
