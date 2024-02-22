186ef7c53b05752918245d6754d46045813a2819
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-02-22 17:56:27 +1100

    The mock_imports.txt file now supports comments at the end of lines, not just the start of lines

--- docs/_modules/mock_imports.py
+++ docs/_modules/mock_imports.py
@@ -6,16 +6,19 @@ comment_symbol = "#"
 mock_imports = []
 project_root = os.path.dirname(os.path.dirname(__file__))
 mock_imports_file_path = os.path.join(project_root, mock_imports_file)
-print(project_root)
-print(mock_imports_file_path)
 
 try:
     with open(mock_imports_file_path, 'r') as file:
         lines = file.read().splitlines()
-        lines = [line.strip() for line in lines] # Strip leading and trailing whitespace from each line
-        lines = [line for line in lines if line] # Ignore blank lines
-        lines = [line for line in lines if not line.startswith(comment_symbol)] # Ignore commented lines
-        mock_imports = lines
+
+        mock_imports = []
+
+        for line in lines:
+            line = line.split(comment_symbol)[0] # Strip text after the comment symbol
+            line = line.strip() # Strip leading and trailing whitespace from each line
+            if line != "": # Ignore blank lines
+                mock_imports.append(line)
+
         print(f"Sourced {len(mock_imports)} mock imports from '{mock_imports_file_path}'")
 except FileNotFoundError:
     print(f"Mock imports file not found at '{mock_imports_file_path}'")
