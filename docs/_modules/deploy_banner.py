061859a67a71148ea6c1dfaa830c47b2bc636ddf
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-03-08 15:56:09 +1100

    Fixed one incorrect URL

--- docs/_modules/deploy_banner.py
+++ docs/_modules/deploy_banner.py
@@ -9,7 +9,7 @@ def banner():
 
     knowledge_hub_url = "https://knowledge.dea.ga.gov.au/"
     github_url = "https://github.com/GeoscienceAustralia/dea-knowledge-hub"
-    deploy_logs_url = "https://app.netlify.com/sites/dea-knowledge-hub/deploys"
+    deploy_logs_url = "https://app.netlify.com/sites/dea-docs/deploys"
 
     pull_request_banner = f'You are viewing <strong>{deploy_name} #{review_id}</strong>, not the official <a href="{knowledge_hub_url}">DEA Knowledge Hub</a>. View the <a href="{deploy_logs_url}/{deploy_id}">Deploy log</a> or <a href="{github_url}/pull/{review_id}">Pull request</a>.'
 
