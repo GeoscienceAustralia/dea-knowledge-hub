01e93df58484a0449f0da20d189d5ab63bb74a45
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-03-14 11:15:51 +1100

    Editing the env variables and preview banner

--- docs/_modules/deploy_banner.py
+++ docs/_modules/deploy_banner.py
@@ -1,13 +1,11 @@
 import os
 
 def banner():
-    deploy_name = os.environ.get("DEPLOY_NAME")
-    review_id = os.environ.get("REVIEW_ID")
-    deploy_id = os.environ.get("DEPLOY_ID")
+    pull_request_number = os.environ.get("PULL_REQUEST_NUMBER")
+    deploy_log_url = os.environ.get("PR_PREVIEW_DEPLOY_LOG_URL")
 
     knowledge_hub_url = "https://knowledge.dea.ga.gov.au/"
     github_url = "https://github.com/GeoscienceAustralia/dea-knowledge-hub"
-    deploy_logs_url = "https://app.netlify.com/sites/dea-docs/deploys"
 
-    return f'You are viewing <strong>{deploy_name} #{review_id}</strong>, not the official <a href="{knowledge_hub_url}">DEA Knowledge Hub</a>. View the <a href="{deploy_logs_url}/{deploy_id}">Deploy log</a> or <a href="{github_url}/pull/{review_id}">Pull request</a>.'
+    return f'You are viewing <strong>Pull Request #{pull_request_number}</strong>, not the official <a href="{knowledge_hub_url}">DEA Knowledge Hub</a>. View the <a href="{github_url}/pull/{pull_request_number}">Pull request</a> or <a href="{deploy_log_url}">Deploy log</a>.'
 
