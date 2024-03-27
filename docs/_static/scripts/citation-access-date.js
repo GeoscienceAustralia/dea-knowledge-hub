425798d979d4e86c0f6c0879898b65a0788932f1
Benji Glitsos <benji.glitsos@ga.gov.au>
2024-03-27 11:21:01 +1100

    Accessed date script is now working

--- docs/_static/scripts/citation-access-date.js
+++ docs/_static/scripts/citation-access-date.js
@@ -1,6 +1,18 @@
 // Append the access date to the citations on product pages
 
 document.addEventListener("DOMContentLoaded", function (event) {
+    function removeNewlineAtEnd(str) {
+        return str.replace(/\n$/, "");
+    }
+
+    const accessedDate = (() => {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        return ` [Accessed ${year}-${month}-${day}]`;
+    })();
+
     const dataCitation = document
         .getElementById("data-citation")
         .querySelector("pre");
@@ -8,13 +20,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
         .getElementById("paper-citation")
         .querySelector("pre");
 
-    const todayInternationalFormat = (() => {
-        const date = new Date();
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-        return `${year}-${month}-${day}`;
-    })();
-
-    dataCitation.textContent += " Accessed: " + todayInternationalFormat;
+    dataCitation.textContent =
+        removeNewlineAtEnd(dataCitation.textContent) + accessedDate;
+    paperCitation.textContent =
+        removeNewlineAtEnd(paperCitation.textContent) + accessedDate;
 });
