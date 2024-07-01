0bf7b2657a4e009050038ad8ca35e222ecc348c5
benji-glitsos-ga <benji.glitsos@ga.gov.au>
2024-07-01 11:12:42 +1000

    Added word 'branch' for clarity

--- docs/_modules/pr_preview.py
+++ docs/_modules/pr_preview.py
@@ -8,5 +8,5 @@ def banner():
     knowledge_hub_url = "https://knowledge.dea.ga.gov.au/"
     github_url = "https://github.com/GeoscienceAustralia/dea-knowledge-hub"
 
-    return f'You are viewing <strong>PR Preview {pull_request_number}</strong>, not the official <a href="{knowledge_hub_url}">DEA Knowledge Hub</a>. View the <a href="{github_url}/pull/{pull_request_number}">Pull request</a> or <a href="{deploy_log_url}">Deploy log</a> for <strong>{pull_request_branch}</strong>.'
+    return f'You are viewing <strong>PR Preview {pull_request_number}</strong>, not the official <a href="{knowledge_hub_url}">DEA Knowledge Hub</a>. View the <a href="{github_url}/pull/{pull_request_number}">Pull request</a> or <a href="{deploy_log_url}">Deploy log</a> for <strong>{pull_request_branch}</strong> branch.'
 
