7c30bf4ce1c9a9d6c642a4f99adfaa6587b479b4
Benji Glitsos <benji.glitsos@ga.gov.au>
2025-02-05 11:28:52 +1100

    Writing comment documentation

--- scripts/validation-reports/__init__.py
+++ scripts/validation-reports/__init__.py
@@ -1,6 +1,14 @@
 # Validation Report scripts
 
-# These scripts are used to generate the Validation Reports which are published to the DEA Knowledge Hub, including Site Reports, Quarterly Reports, and Yearly Reports. <https://knowledge.dea.ga.gov.au/validation/>
+# These scripts are used to generate the Validation Reports which are published to the DEA Knowledge Hub, including Site Reports, Quarterly Reports, and Yearly Reports.
+
+# <https://knowledge.dea.ga.gov.au/validation/>
+
+# How to use this script:
+
+# 1. In this directory, run:
+#     python __init__.py
+# 2. The output will be generated, containing Markdown, Image, and CSV files.
 
 from . import AddSite
 from . import SingleDualPlot
