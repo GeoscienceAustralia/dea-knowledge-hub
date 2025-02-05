99e750f7a695b3eb355f1b875daabff4cd888902
Benji Glitsos <benji.glitsos@ga.gov.au>
2025-02-05 11:24:54 +1100

    Writing comment documentation

--- scripts/validation-reports/__init__.py
+++ scripts/validation-reports/__init__.py
@@ -1,3 +1,7 @@
+# Validation Report scripts
+
+# These scripts are used to generate the Validation Reports which are published to the DEA Knowledge Hub, including Site Reports, Quarterly Reports, and Yearly Reports. <https://knowledge.dea.ga.gov.au/validation/>
+
 from . import AddSite
 from . import SingleDualPlot
 from . import rgbPlot
