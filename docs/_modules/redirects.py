d954969f76505692d1739d2637e2f972b9d17999
Benji Glitsos <benji.glitsos@ga.gov.au>
2025-02-21 14:18:39 +1100

    Documenting redirects function

--- docs/_modules/redirects.py
+++ docs/_modules/redirects.py
@@ -2,6 +2,9 @@ import glob
 import re
 
 def source_redirects_in_custom_format(file_paths_glob):
+    """
+    Sources redirects from a filepath glob (such as "_redirects/*.txt"). This enables splitting the redirects into multiple files. Also, it uses custom format for redirects which is slightly different from the rediraffe format. It doesn't require quote marks and it supports comments by starting the line with a '# '.
+    """ 
     comment_syntax = r"# .*"
     redirect_syntax = r"\"?([^\"\s]*)\"?\s*\"?([^\"\s]*)\"?"
     redirects = {}
