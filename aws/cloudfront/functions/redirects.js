8de9cde1cfeac3bf401f60f31ea110068eac871a
Benji Glitsos <benji.glitsos@ga.gov.au>
2025-03-31 10:50:15 +1100

    Code comment

--- aws/cloudfront/functions/redirects.js
+++ aws/cloudfront/functions/redirects.js
@@ -2,7 +2,7 @@
 // Learn how to test and deploy these redirects:
 // https://docs.dev.dea.ga.gov.au/public_services/dea_knowledge_hub/aws_redirects.html
 
-// This function handles redirect logic
+// This function handles redirect logic.  Note: The redirects are sequenced in a specific order.
 
 async function handler(event) {
     const request = event.request;
