7bc1c80016cbadac7d1734398abab3053d76f3c9
Benji Glitsos <benji.glitsos@ga.gov.au>
2025-04-08 13:42:45 +1000

    Making code more browser compatible

--- docs/_static/scripts/google-analytics-access-link-events.js
+++ docs/_static/scripts/google-analytics-access-link-events.js
@@ -1,9 +1,13 @@
-document.querySelectorAll(".access-link").forEach(function(link) {
-    link.addEventListener("click", function(event) {
-        gtag("event", "click", {
-            "category": "access-link",
-            "label": event.target.id,
-            "value": event.target.href,
+document.addEventListener('DOMContentLoaded', function() {
+    var accessLinks = document.querySelectorAll(".access-link");
+
+    for (var i = 0; i < accessLinks.length; i++) {
+        accessLinks[i].addEventListener("click", function(event) {
+            gtag("event", "click", {
+                "category": "access-link",
+                "label": event.target.id,
+                "value": event.target.href,
+            });
         });
-    });
+    }
 });
