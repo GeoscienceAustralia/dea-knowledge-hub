17a0189b864fe42ddebce42d9a17092f553806c6
Benji Glitsos <benji.glitsos@ga.gov.au>
2025-04-08 13:36:35 +1000

    Removing the setTimeout for now

--- docs/_static/scripts/google-analytics-access-link-events.js
+++ docs/_static/scripts/google-analytics-access-link-events.js
@@ -1,13 +1,9 @@
 document.querySelectorAll(".access-link").forEach(function(link) {
     link.addEventListener("click", function(event) {
-        gtag("event", "click_access_link", {
-            "link_type": event.target.id,
-            "link_url": event.target.href,
+        gtag("event", "click", {
+            "category": "access-link",
+            "label": event.target.id,
+            "value": event.target.href,
         });
-
-        // Ensure the event is sent before the page unloads by delaying for 0.3 seconds
-        setTimeout(function() {
-          window.location.href = url;
-        }, 300);
     });
 });
