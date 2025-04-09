5faee833b5bb1c2f99fd66a7073d8528cd205d45
Benji Glitsos <benji.glitsos@ga.gov.au>
2025-04-09 15:11:49 +1000

    Updating queryselector for new CSS class format

--- docs/_static/scripts/google-analytics-access-link-events.js
+++ docs/_static/scripts/google-analytics-access-link-events.js
@@ -1,7 +1,7 @@
 // If a class of the format 'access-link--something' exists in a classList, return 'something'.
 function labelFromClassList(classList) {
     let list = Array.prototype.slice.call(classList)
-    var prefix = "access-list--";
+    var prefix = "access-link--";
 
     for (var i = 0; i < list.length; i++) {
         if ([i].indexOf(prefix) === 0) {
@@ -13,7 +13,7 @@ function labelFromClassList(classList) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    var accessLinks = document.querySelectorAll(".access-link");
+    var accessLinks = document.querySelectorAll("[class^='access-link--']");
 
     for (var i = 0; i < accessLinks.length; i++) {
         accessLinks[i].addEventListener("click", function(event) {
