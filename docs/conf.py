afdacb65b73c8db23133a3d0fdb0727d7d86b29b
Benji Glitsos <benji.glitsos@ga.gov.au>
2025-04-23 13:11:03 +1000

    Switching to sphinx-reredirects

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ extensions = [
     "myst_parser",
     "nbsphinx",
     "sphinx_design",
-    "sphinxext.rediraffe",
+    "sphinx-reredirects",
     "sphinxcontrib.datatemplates",
     "sphinx_external_toc",
     "sphinx_sitemap",
@@ -98,7 +98,7 @@ external_toc_path = "table_of_contents.yaml"
 
 if (
     is_production or is_pr_preview or enable_redirects
-): rediraffe_redirects = redirects.source_redirects_glob_in_custom_format("_redirects/*.txt")
+): redirects = redirects.source_redirects_glob_in_custom_format("_redirects/*.txt")
 
 sitemap_url_scheme = "{link}"
 
